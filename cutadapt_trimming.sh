210e7d89fcffee18b784a66f299dabe20592dcd4
Tarek-Badr <43323534+Tarek-Badr@users.noreply.github.com>
2021-09-09 15:19:21 +0200

    Update cutadapt_trimming.sh

--- cutadapt_trimming.sh
+++ cutadapt_trimming.sh
@@ -7,7 +7,7 @@ do
     
     cutadapt -a ^GTGCCAGCMGCCGCGGTAA...ATTAGAWACCCBDGTAGTCC \
     -A ^GGACTACHVGGGTWTCTAAT...TTACCGCGGCKGCTGGCAC \
-    -m 215 -M 285 --discard-untrimmed \
+    -m 200 -M 235 --discard-untrimmed \
     -o ${sample}_sub_R1_trimmed.fq.gz -p ${sample}_sub_R2_trimmed.fq.gz \
     ${sample}_sub_R1.fq ${sample}_sub_R2.fq \
     >> cutadapt_primer_trimming_stats.txt 2>&1
